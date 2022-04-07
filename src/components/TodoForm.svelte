585d54ce808d5274a7ebb845157cbba4b72086ec
minjoo <canon.minjoo@gmail.com>
2022-04-07 18:20:30 +0900

    add todo form

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { addTodo } from '../stores/todoStore';
+	let todo = '';
+	const handleSubmit = () => {
+		addTodo(todo);
+		todo = '';
+	};
+</script>
+
+<form on:submit|preventDefault={handleSubmit} class="my-6">
+	<div class="flex flex-col text-sm mb-2">
+		<label for="todo" class="font-bold mb-2">
+			<input
+				type="text"
+				bind:value={todo}
+				name="todo"
+				placeholder="Wattacha gotta do?"
+				class="w-full appearance-none shadow-sm border border-gray-200 p-2 foucs:outline-none foucus:border-gray-500 rounded-lg"
+			/>
+		</label>
+	</div>
+	<button
+		type="submit"
+		class="w-full shadow-sm bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg"
+		>보내기</button
+	>
+</form>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import TodoForm from '../components/TodoForm.svelte';
+	import Todo from '../components/Todo.svelte';
+	import { todos } from '../stores/todoStore';
+</script>
+
+<main>
+	<h1 class="mt-12 text-2xl font-bold text-center text-black">My Todos</h1>
+	<TodoForm />
+	{#each $todos as todo}
+		<Todo {todo} />
+	{/each}
+</main>
